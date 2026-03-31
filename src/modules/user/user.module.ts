5aeb134d43eb22c7d5ba96a861c80c385c346d6a
Dev-Soares <bernardo.soares30@outlook.com>
2026-03-31 16:07:20 -0300

    feat: applying migration to neon database

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,10 +4,11 @@ import { UserController } from './user.controller';
 import { DatabaseModule } from '../database/database.module';
 import { HashModule } from 'src/common/hash/hash.module';
 import { AuthGuardModule } from 'src/common/guards/auth/auth.module';
+import { PlanService } from './plan.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PlanService],
   imports: [DatabaseModule, HashModule, AuthGuardModule],
   exports: [UserService],
 })
