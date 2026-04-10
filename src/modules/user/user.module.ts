29b7911515d000697acf9bfc4c1acdfbaa6c4d2a
Dev-Soares <bernardo.soares30@outlook.com>
2026-04-09 21:17:23 -0300

    fix: fixing hash

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,11 +4,10 @@ import { UserController } from './user.controller';
 import { DatabaseModule } from '../database/database.module';
 import { HashModule } from 'src/common/hash/hash.module';
 import { AuthGuardModule } from 'src/common/guards/auth/auth.module';
-import { PlanService } from './plan.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PlanService],
+  providers: [UserService],
   imports: [DatabaseModule, HashModule, AuthGuardModule],
   exports: [UserService],
 })
